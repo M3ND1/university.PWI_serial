697cfac4e1adfcd8fa4e83a31c34771e3dfdc800
Burtosz <ul0255172@edu.uni.lodz.pl>
2023-05-21 04:09:07 +0200

    Fix. Teraz dziala, jak chcesz zobaczyc co to patrz na kod xD

--- pwi_serial/main.cpp
+++ pwi_serial/main.cpp
@@ -33,63 +33,56 @@
 
     Tserial_event *com;
     std::mutex mutexDataIn;
-    std::vector<std::string> data_in;
-    std::mutex mutexReceivedOrders;
-    std::queue<std::pair<std::string,std::string>> receivedOrders; //struct w ktorym bedzie time string
-    std::queue<std::string> timeVector;
-    std::mutex mutexTimeVector;
+    std::mutex mutextimeQueue;
     std::mutex mutexOrdersToSend;
     std::mutex mutexFirstCommand;
-    std::queue<std::string> ordersToSend;
+    std::mutex mutexReceivedOrders;
+    std::mutex mutexLogtx;
     std::string firstCommand;
-    std::thread *sendThread;
-    std::thread *interpretThread;
-
-
+    std::vector<std::string> logtxVector;
+    std::vector<std::string> dataIn;
+    std::pair<std::string,std::string> firstReceive;
+    std::queue<std::pair<std::string,std::string>> receivedOrders; //struct w ktorym bedzie time string
+    std::queue<std::string> timeQueue;
+    std::queue<std::string> ordersToSend;
     bool sent= false;
+    gboolean threadsWork = FALSE;
 
 // ========================================================================================================================
 
 void logtx (std::string str);
 void parseString(std::string& stringToParse);
-void send_command();
+void sendCommand();
 std::string getCurrentTimestamp();
-void INTERPRET_THREAD();
-void SEND_THREAD();
-void checkOrders();
-void orderInterpreter();
+gboolean checkOrders(gpointer data);
+gboolean orderInterpreter(gpointer data);
 
 // ========================================================================================================================
 
 /* ======================================================== */
 /* ===============  OnCharArrival     ===================== */
 /* ======================================================== */
-void OnDataArrival(int size, char *buffer)
+void onDataArrival(int size, char *buffer)
 {
     std::lock_guard<std::mutex> guard(mutexDataIn);
     if ((size>0) && (buffer!=0))
     {
 
         buffer[size] = 0;
-//        logtx("OnDataArrival: " + std::string(buffer));
-        gtk_label_set_text(GTK_LABEL(gtk_label_1), buffer);
-        data_in.push_back(std::string(buffer));
+        //gtk_label_set_text(GTK_LABEL(gtk_label_1), buffer);
+        dataIn.push_back(std::string(buffer));
         if(*buffer=='\r')
         {
-            std::lock_guard<std::mutex> guard2(mutexTimeVector);
-            timeVector.push("time"/*getCurrentTimestamp()*/);
+            std::lock_guard<std::mutex> guard2(mutextimeQueue);
+            timeQueue.push(getCurrentTimestamp());
         }
-
     }
-
-    //std::vector<std::string> orders_tmp = split(data_in, '\n');
-    //orders.insert(orders.end(), orders_tmp.begin(), orders_tmp.end())
 }
 
 /* ======================================================== */
 /* ===============  OnCharArrival     ===================== */
 /* ======================================================== */
-void SerialEventManager(void* object, uint32 event)    // BYLO: void SerialEventManager(uint32 object, uint32 event)
+void serialEventManager(void* object, uint32 event)    // BYLO: void serialEventManager(uint32 object, uint32 event)
 {
     char *buffer;
     int   size;
@@ -101,15 +94,14 @@ void SerialEventManager(void* object, uint32 event)    // BYLO: void SerialEvent
         switch(event)
         {
             case  SERIAL_CONNECTED  :
-                                        logtx("Connected ! \n");
-
-                                       sendThread->join();
-                                       interpretThread->join();
+                                        logtx(" Connected ! \n");
+                                        threadsWork =TRUE;
+                                        g_timeout_add(1000,orderInterpreter,gtk_text_view_1);
+                                        g_timeout_add(1000,checkOrders,gtk_text_view_1);
                                         break;
             case  SERIAL_DISCONNECTED  :
                                         logtx("Disonnected ! \n");
-                                        sendThread->detach();
-                                        interpretThread->detach();
+                                        threadsWork =FALSE;
                                         break;
             case  SERIAL_DATA_SENT  :
                                         logtx("Data sent ! \n");
@@ -127,7 +119,7 @@ void SerialEventManager(void* object, uint32 event)    // BYLO: void SerialEvent
                                         logtx("SERIAL_DATA_ARRIVAL\n");
                                         size   = com->getDataInSize();
                                         buffer = com->getDataInBuffer();
-                                        OnDataArrival(size, buffer);
+                                        onDataArrival(size, buffer);
                                         com->dataHasBeenRead();
                                         break;
         }
@@ -135,26 +127,37 @@ void SerialEventManager(void* object, uint32 event)    // BYLO: void SerialEvent
 }
 std::string getCurrentTimestamp()
 {
-    using std::chrono::system_clock;
     auto currentTime = std::chrono::system_clock::now();
-    char buffer[80];
+    auto currentTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(
+        currentTime.time_since_epoch()
+    );
+    std::time_t tt = std::chrono::system_clock::to_time_t(currentTime);
+    std::tm timeInfo;
+    localtime_s(&timeInfo, &tt);
 
-    auto transformed = currentTime.time_since_epoch().count() / 1000000;
+    std::stringstream ss;
+    ss << std::put_time(&timeInfo, "%Y-%m-%d %H:%M:%S");
+    ss << ":" << std::setfill('0') << std::setw(3) << currentTimeMs.count() % 1000;
 
-    auto millis = transformed % 1000;
-
-    std::time_t tt;
-    tt = system_clock::to_time_t(currentTime);
-    auto timeinfo = localtime(&tt);
-    strftime(buffer, 80, "%F %H:%M:%S", timeinfo);
-    sprintf(buffer, "%s:%03d", buffer, (int)millis);
-
-    return std::string(buffer);
+    return ss.str();
 }
 
 // ========================================================================================================================
 
-void logtx (std::string str) {
+void logtx(std::string str)
+{
+    std::lock_guard<std::mutex> guard(mutexLogtx);
+    logtxVector.push_back(getCurrentTimestamp()+' '+str);
+}
+
+
+gboolean logtxgtk (gpointer data) {
+
+    std::lock_guard<std::mutex> guard(mutexLogtx);
+    std::string str;
+    for(auto& i:logtxVector)
+        str+=i;
+    logtxVector.clear();
 
     GtkTextBuffer *buffer;
     GtkTextIter iter;
@@ -162,7 +165,8 @@ void logtx (std::string str) {
     buffer = gtk_text_view_get_buffer ( GTK_TEXT_VIEW(gtk_text_view_1) );
 
     gtk_text_buffer_get_iter_at_offset (buffer, &iter, 0);
-    gtk_text_buffer_insert (buffer, &iter, std::string(str + "\n").c_str(), -1);
+    gtk_text_buffer_insert (buffer, &iter, std::string(str).c_str(), -1);
+    return TRUE;
 }
 
 // ========================================================================================================================
@@ -171,31 +175,26 @@ void logtx (std::string str) {
 extern "C" G_MODULE_EXPORT void on_window_main_destroy () {
 
     // zakonczenie polaczenia i skasowanie obiektu
-    com->disconnect();
-    delete com;
-    com = 0;
-
-    //deleting threads
-    delete sendThread ;
-    delete interpretThread ;
-    sendThread =0;
-    interpretThread =0;
+    if(com)
+    {
+        com->disconnect();
+        delete com;
+        com = 0;
+    }
 
     gtk_main_quit ();
 }
-//
 void orderReceivedBuilder()
 {
     std::lock_guard<std::mutex> guard(mutexDataIn);
-    //std::lock_guard<std::mutex> guardQueue(mutexReceivedOrders); ///jesli cos nie dziala to przenies mutex do parseString
-    std::string temp = "";
-    for(int i=0;i<data_in.size();i++)
-        temp += data_in[i];
+    std::string temp;
+    for(int i=0;i<dataIn.size();i++)
+        temp += dataIn[i];
 
-    //clear data_in
-    data_in.clear();
+    //clear dataIn
+    dataIn.clear();
     parseString(temp);
-    data_in.push_back(temp);
+    dataIn.push_back(temp);
 }
 //===
 void parseString(std::string& stringToParse)
@@ -205,43 +204,38 @@ void parseString(std::string& stringToParse)
     size_t i=0;
     while((delimiterIndex = stringToParse.find(delimiter)) != std::string::npos)
     {
-        std::lock_guard<std::mutex> guard(mutexTimeVector);
+        std::lock_guard<std::mutex> guard(mutextimeQueue);
 
         std::string order = stringToParse.substr(0,delimiterIndex-1);
-        receivedOrders.push(std::make_pair(order, timeVector.front()));
-        timeVector.pop();
+        receivedOrders.push(std::make_pair(order, timeQueue.front()));
+        timeQueue.pop();
         stringToParse.erase(0, delimiterIndex+1);
     }
 }
-//bool checkCommand(const std::string& com)
-//{
-//    std::vector<std::string> v = {"STA", "RD", "LED", "NBS"};
-//    ///po jednej literce
-//
-//    return (std::find(v.begin(), v.end(), com) != v.end()) ? true : false;
-//}
 
 // ========================================================================================================================
 
-void on_gtk_button_1_clicked (GtkButton *button, gpointer user_data) {
-    //com->sendData("Hello World\n",sizeof("Hello World\n"));
-//    logtx ("len: " + std::to_string(data_in.size()));a
-//    for (unsigned int idx = 0; idx < data_in.size(); idx++)
-//        logtx(data_in[idx]);
-
-//    std::lock_guard<std::mutex> guard(mutexDataIn);
-//    data_in.clear();
-    //std::string command = std::string (gtk_entry_get_text(GTK_ENTRY(gtk_entry_1))); ///pobierz komende z pola tekstowego
-    std::lock_guard<std::mutex> guard(mutexOrdersToSend);
-    if(ordersToSend.empty())//checkCommand(command)) ///sprawdz czy komenda jest poprawna
+void on_gtk_button_1_clicked (GtkButton *button, gpointer user_data)
+{
+//    if(!threadsWork)
+//    {
+//        threadsWork= TRUE;
+//        g_timeout_add(1000,orderInterpreter,gtk_text_view_1);
+//        g_timeout_add(1000,checkOrders,gtk_text_view_1);
+//    }
+//    else{
+//        threadsWork=FALSE;
+//    }
+    if(ordersToSend.empty())
     {
-        int n=100;
 
-        //ordersToSend.push("STA\n\r");
+        int n = std::atoi(std::string (gtk_entry_get_text(GTK_ENTRY(gtk_entry_1))).c_str());
+        if(!n) n=100;
+        std::lock_guard<std::mutex> guard(mutexOrdersToSend);
         ordersToSend.push("LED0\n\r");
         ordersToSend.push("LED1\n\r");
-
-//        ordersToSend.push("NBS\n\r");
+        ordersToSend.push("NBS\n\r");
+        gtk_label_set_text(GTK_LABEL(gtk_label_1), (std::string("Ilosc RD "+std::to_string(n))).c_str());
         while(n--)
         {
             ordersToSend.push("RD1\n\r");
@@ -249,19 +243,12 @@ void on_gtk_button_1_clicked (GtkButton *button, gpointer user_data) {
         ordersToSend.push("LED0\n\r");
         gtk_entry_set_text(GTK_ENTRY(gtk_entry_1),"");
     }
-    // int n=100;
-    // while(n--)
-    // {
-    //     checkOrders();
-    //     orderInterpreter();
-    //     Sleep(200);
-    // }
 }
 
-void orderInterpreter()
+gboolean orderInterpreter(gpointer data)
 {
     std::lock_guard<std::mutex> guard(mutexReceivedOrders);
-    logtx("jestem w orderInterpreter, sprawdzam if\n" /*+  getCurrentTimestamp() +"\n"*/);
+    //logtx("jestem w orderInterpreter, sprawdzam if\n");
     if(!receivedOrders.empty())
     {
         std::string receivedOrderData = receivedOrders.front().first;
@@ -280,61 +267,43 @@ void orderInterpreter()
         }
         std::lock_guard<std::mutex> guard2(mutexFirstCommand);
         firstCommand.clear();
-        sent = false;
         }
     else{
-        logtx("jestem w orderInterpreter, else chce zbudowac ordery\n");
+        //logtx("jestem w orderInterpreter, else chce zbudowac ordery\n");
         orderReceivedBuilder();
     }
-
+return threadsWork;
 
 }
 
-void checkOrders() //wywolywac co jakis czas zeby wysylac ordery
+gboolean checkOrders(gpointer data) //wywolywac co jakis czas zeby wysylac ordery
 {
     std::lock_guard<std::mutex> guard2(mutexFirstCommand);
     std::lock_guard<std::mutex> guard(mutexOrdersToSend);
-    logtx("jestem w chceckOrders, sprawdzam if\n" /*+  getCurrentTimestamp() +"\n"*/);
+    //logtx("jestem w chceckOrders, sprawdzam if\n");
     if(firstCommand.empty() && !ordersToSend.empty())
     {
-        logtx("jestem w chceckOrders, if, number of orders to send:"+ std::to_string(ordersToSend.size()) +"\n");
+        //logtx("jestem w chceckOrders, if, number of orders to send:"+ std::to_string(ordersToSend.size()) +"\n");
         firstCommand = ordersToSend.front();
-//        sent=false;
-        logtx("jestem w chceckOrders w if: first command= "+firstCommand+"\n");
+        logtx("firstCommand: " + firstCommand);
+        sent=false;
+        //logtx("jestem w chceckOrders w if: first command= "+firstCommand+"\n");
         ordersToSend.pop();
     }
-    if(!ordersToSend.empty()) logtx("jestem w chceckOrders number of orders to send:"+ std::to_string(ordersToSend.size()) +"\n");
+    //if(!ordersToSend.empty()) logtx("jestem w chceckOrders number of orders to send:"+ std::to_string(ordersToSend.size()) +"\n");
     if(!sent)
     {
-        logtx("jestem w chceckOrders, sprawdzam if sent, nie wyslalo wiec wysylam\n");
-        send_command();
+        //logtx("jestem w chceckOrders, sprawdzam if sent, nie wyslalo wiec wysylam\n");
+        sendCommand();
     }
+    return threadsWork;
 }
 
-void send_command()
+void sendCommand()
 {
-    //std::lock_guard<std::mutex> guard(mutexFirstCommand);
     char* buf = const_cast<char*>(firstCommand.c_str());
     com->sendData(buf,sizeof(buf));
     sent=true;
-}
-
-void SEND_THREAD()
-{
-    while(1)
-    {
-        checkOrders();
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    }
-}
-
-void INTERPRET_THREAD()
-{
-    while(1)
-    {
-        orderInterpreter();
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    }
 }
 
 // ========================================================================================================================
@@ -374,15 +343,13 @@ int main( int argc, char *argv[]) {
     com = new Tserial_event();
     if (com!=0)
     {
-        com->setManager(SerialEventManager);
+        com->setManager(serialEventManager);
         error = com->connect("COM5", 115200, SERIAL_PARITY_NONE, 8, true);
         if (!error)
         {
             //com->sendData("Hello World\n");
             com->setRxSize(1);
 
-           sendThread = new std::thread (SEND_THREAD);
-           interpretThread = new std::thread(INTERPRET_THREAD);
         }
         else
             logtx("ERROR : com->connect (" + std::to_string(error) + ")\n");
@@ -390,7 +357,7 @@ int main( int argc, char *argv[]) {
     }
 
 // ========================================================================================================================
-
+    g_idle_add(logtxgtk,gtk_text_view_1);
     gtk_main(); // w tej petli program okienkowy tkwi, dop�ki go nie zakonczymy przyciskiem 'X'
 
 
